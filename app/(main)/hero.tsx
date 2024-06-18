cd8d709c5fd65b63d1c77ecd4c937ab17803af01
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-06-18 22:16:35 +0000

    chore: add badge

--- app/(main)/hero.tsx
+++ app/(main)/hero.tsx
@@ -26,6 +26,7 @@ export const Hero = () => {
           <h3 className="text-2xl font-light opacity-70">
             <Balancer>social bookmarking for everyone</Balancer>
           </h3>
+          <Image className="cursor-pointer bg-black mt-4" src="/gaudmire.svg" width={140} height={100} alt="n&w s5" />
           <div className="my-8 h-96 not-prose w-full overflow-hidden border rounded-lg md:rounded-xl md:h-[520px]">
             <Image
               className="h-full w-full object-cover object-bottom"
