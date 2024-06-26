018c96e2bf0dd7f3b96d43252b1250c4afb1eda9
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-06-26 21:17:03 +0000

    chore: better icons

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -1,26 +1,22 @@
 import { Section, Container } from "~/components/blocks";
-import { Github, Twitter } from "lucide-react";
-import { Button } from "~/components/primitives/button";
+import { Github } from "~/components/icons/github";
+import { Twitter } from "~/components/icons/twitter";
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="mt-32">
       <Section>
         <Container className="not-prose flex flex-col md:flex-row md:gap-2 gap-6 justify-between md:items-center">
-          <div className="flex gap-2">
+          <div className="flex gap-4">
             <a href="https://github.com/ephraimduncan" target="_blank">
-              <Button>
-                <Github />
-              </Button>
+              <Github />
             </a>
             <a href="https://x.com/ephraimduncan" target="_blank">
-              <Button>
-                <Twitter />
-              </Button>
+              <Twitter />
             </a>
           </div>
           <p className="text-muted-foreground">
-            © <a href="https://github.com/ephraimduncan">Ephraim Duncan</a>. All rights reserved. 2024-present.
+            © <a href="https://duncan.land">Ephraim Duncan</a>. All rights reserved. 2024-present.
           </p>
         </Container>
       </Section>
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div>
       <Hero />
       <FeatureSection />
-      <CTA />
+      {/* <CTA />  */}
       <Footer />
     </div>
   );
